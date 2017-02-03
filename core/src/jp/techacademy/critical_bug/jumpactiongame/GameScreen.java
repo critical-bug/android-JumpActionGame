431bcc1629d737f9c7e9f9251fc3a8cbb014b28f
critical-bug <critical-bug@users.noreply.github.com>
2017-02-03 10:29:06 +0000

    GameScreen で背景表示

---
+++ core/src/jp/techacademy/critical_bug/jumpactiongame/GameScreen.java
@@ -0,0 +1,54 @@
+package jp.techacademy.critical_bug.jumpactiongame;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+public class GameScreen extends ScreenAdapter {
+    private static final float CAMERA_HEIGHT = 15;
+    private static final float CAMERA_WIDTH = 10;
+    private final JumpActionGame mGame;
+    private final Sprite mBg;
+    private final OrthographicCamera mCamera;
+    private final FitViewport mViewPort;
+
+    public GameScreen(final JumpActionGame game) {
+        mGame = game;
+        Texture bgTexture = new Texture("back.png");
+        // TextureRegionで切り出す時の原点は左上
+        mBg = new Sprite( new TextureRegion(bgTexture, 0, 0, 540, 810));
+        mBg.setSize(CAMERA_WIDTH, CAMERA_HEIGHT);
+        mBg.setPosition(0, 0);
+
+        mCamera = new OrthographicCamera();
+        mCamera.setToOrtho(false, CAMERA_WIDTH, CAMERA_HEIGHT);
+        mViewPort = new FitViewport(CAMERA_WIDTH, CAMERA_HEIGHT);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // カメラの座標をアップデート（計算）し、スプライトの表示に反映させる
+        mCamera.update();
+        mGame.batch.setProjectionMatrix(mCamera.combined);
+
+        mGame.batch.begin();
+        // 原点は左下
+        mBg.setPosition(mCamera.position.x - CAMERA_WIDTH / 2, mCamera.position.y - CAMERA_HEIGHT / 2);
+        mBg.draw(mGame.batch);
+        mGame.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        mViewPort.update(width, height);
+    }
+}
